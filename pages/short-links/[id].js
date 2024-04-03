221032747241af3e67f59dc3566829ad9489af5c
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-04-03 15:53:21 +0900

    feat: 짧은 주소 수정, 삭제하기

--- pages/short-links/[id].js
+++ pages/short-links/[id].js
@@ -1,8 +1,32 @@
-import Head from 'next/head';
-import ShortLinkForm, { ShortLinkFormType } from '@/components/ShortLinkForm';
-import styles from '@/styles/ShortLinkEditPage.module.css';
+import Head from "next/head";
+import ShortLinkForm, { ShortLinkFormType } from "@/components/ShortLinkForm";
+import styles from "@/styles/ShortLinkEditPage.module.css";
+import ShortLink from "@/db/models/ShortLink";
+import { useRouter } from "next/router";
+import axios from "@/lib/axios";
 
-export default function ShortLinkEditPage() {
+export async function getServerSideProps() {
+  await dbConnect();
+  const shortLinks = await ShortLink.findById(id);
+  if (shortLinks) {
+    return {
+      props: {
+        shortLinks: JSON.parse(JSON.stringify(shortLinks)),
+      },
+    };
+  }
+  return {
+    notFound: true,
+  };
+}
+
+export default function ShortLinkEditPage({ shortLink }) {
+  const router = useRouter();
+  const { id } = router.query;
+  async function handleSubmit(values) {
+    await axios.patch(`/short=links/${id}`, values);
+    router.push(`/short-links/`);
+  }
   return (
     <>
       <Head>
@@ -10,7 +34,11 @@ export default function ShortLinkEditPage() {
       </Head>
       <div className={styles.page}>
         <h1 className={styles.title}>수정하기</h1>
-        <ShortLinkForm type={ShortLinkFormType.Edit} />
+        <ShortLinkForm
+          type={ShortLinkFormType.Edit}
+          initialValues={shortLink}
+          onSubmit={handleSubmit}
+        />
       </div>
     </>
   );
